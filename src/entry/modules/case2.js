19368add8eb2693948630f7cb265a89bc75265e1
lixinliang <lixinliang@yy.com>
2016-09-14 15:57:09 +0800

    update
    update case2 and 3

--- src/entry/modules/case2.js
+++ src/entry/modules/case2.js
@@ -1,16 +1,32 @@
 import timer from './timer';
 
+// return src or image data
+let getData = (function () {
+    let canvas = document.createElement('canvas');
+    canvas.width = 500;
+    canvas.height = 500;
+    let context = canvas.getContext('2d');
+    return function getData ( img ) {
+        return img.src;
+        // context.clearRect(0, 0, 500, 500);
+        // context.drawImage(img, 0, 0, 500, 500);
+        // return canvas.toDataURL();
+    }
+})();
+
 module.exports = function ( $ ) {
+    // case2
     // click download
     $('.download').on('click', function () {
         let url = './img/frame-animation/rose/$.png';
+        let source = [];
         let pic = 60;
         let current = 0;
         let progress = $('.download-progress i');
         // start download
         timer.start('download');
         for (let i = 0; i < pic; ) {
-            let img = new Image;
+            let img = source[i] = new Image;
             img.onload = onload;
             img.src = url.replace('$', ('0' + (++i)).slice(-2));
         }
@@ -25,8 +41,28 @@ module.exports = function ( $ ) {
                     this.off('click', arguments.callee, false);
                     // start play
                     timer.start('play');
-                    // TODO: 生成关键帧
-                    // TODO: 尝试采用压缩图
+                    // create keyframes
+                    $('.player').innerHTML = `
+                    <style>
+                        .player {
+                            animation: play 4s steps(${ pic }) infinite;
+                            -webkit-animation: play 4s steps(${ pic }) infinite;
+                        }
+                        @keyframes play {
+                            ${ $for(pic, ( index ) => `
+                                ${ ((index + 1)*100/pic).toFixed(4) }% {
+                                    background-image: url(${ getData(source[index]) })
+                                }
+                            `) }
+                        }
+                        @-webkit-keyframes play {
+                            ${ $for(pic, ( index ) => `
+                                ${ ((index + 1)*100/pic).toFixed(4) }% {
+                                    background-image: url(${ getData(source[index]) })
+                                }
+                            `) }
+                        }
+                    </style>`;
                     // end play
                     let playTime = timer.end('play');
                     $('.play-time span').innerHTML = playTime/1000 + 's';
@@ -36,3 +72,12 @@ module.exports = function ( $ ) {
         }
     }, false);
 };
+
+// iterator
+function $for ( iterator, template ) {
+    let result = [];
+    for (let i = 0; i < iterator; i++) {
+        result.push(template(i));
+    }
+    return result.join('');
+}
--- src/entry/modules/case3.js
+++ src/entry/modules/case3.js
@@ -1,4 +1,20 @@
-module.exports = function () {
-    console.log('case3');
+import JSZip from 'jszip';
+import JSZipUtils from 'jszip-utils';
+
+module.exports = function ( $ ) {
+    // case3
+    $('.download').on('click', function () {
+        this.off('click', arguments.callee, false);
+        JSZipUtils.getBinaryContent('./img/svga/rose.svga', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            JSZip.loadAsync(data).then((zip) => {
+                zip.file('movie.spec').async('string').then((spec) => {
+                    let movieItem = JSON.parse(spec);
+                    console.log(movieItem, zip);
+                });
+            });
+        });
+    }, false);
 };
-// TODO: 尝试解压但不用canvas播放
